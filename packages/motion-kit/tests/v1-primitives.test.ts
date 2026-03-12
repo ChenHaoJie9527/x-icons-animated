58530e2c23e257c7c6327085ca8a38919c946944
ChenHaoJie9527 <a18520145493@163.com>
2026-03-12 15:30:00 +0800

    feat: 对v1-primitives的rotate函数进行单元测试

--- packages/motion-kit/tests/v1-primitives.test.ts
+++ packages/motion-kit/tests/v1-primitives.test.ts
@@ -1,4 +1,4 @@
-import { fade } from "../src/primitives";
+import { fade, rotate } from "../src/primitives";
 import { expect, it, describe } from "vitest";
 
 describe("motion-kit primitives", () => {
@@ -14,13 +14,37 @@ describe("motion-kit primitives", () => {
 			});
 		});
 
-		it("当提供参数时使用参数值", () => {
+		it("当提供参数时使用参数值作为目标值", () => {
 			expect(fade(0, 1)).toEqual({
 				normal: {
 					opacity: 0,
 				},
 				animate: {
 					opacity: 1,
+				},
+			});
+		});
+	});
+
+	describe("rotate", () => {
+		it("当提供参数时使用参数值作为目标值", () => {
+			expect(rotate(0, 45)).toEqual({
+				normal: {
+					rotate: 0,
+				},
+				animate: {
+					rotate: 45,
+				},
+			});
+		});
+
+		it("第二个参数为数组时使用数组中的值作为目标值", () => {
+			expect(rotate(0, [0, 45, 90])).toEqual({
+				normal: {
+					rotate: 0,
+				},
+				animate: {
+					rotate: [0, 45, 90],
 				},
 			});
 		});
