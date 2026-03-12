f78a07c4c2e1365a875521b26118263febb4d201
ChenHaoJie9527 <a18520145493@163.com>
2026-03-12 15:46:11 +0800

    feat: 对v1-primitives的scaleXY函数进行单元测试

--- packages/motion-kit/src/primitives.ts
+++ packages/motion-kit/src/primitives.ts
@@ -11,10 +11,27 @@ const createPatch = (
 	};
 };
 
+/**
+ * @description 淡入淡出
+ * @param from 初始透明度
+ * @param to 目标透明度
+ * @example
+ * fade(0, 1) // { normal: { opacity: 0 }, animate: { opacity: 1 } }
+ * @returns DefaultMotionPatch
+ */
 export const fade = (from = 1, to = 1): DefaultMotionPatch => {
 	return createPatch({ opacity: from }, { opacity: to });
 };
 
+/**
+ * @description 旋转
+ * @param from 初始旋转角度
+ * @param to 目标旋转角度
+ * @example
+ * rotate(0, 45) // { normal: { rotate: 0 }, animate: { rotate: 45 } }
+ * rotate(0, [0, 45, 90]) // { normal: { rotate: 0 }, animate: { rotate: [0, 45, 90] } }
+ * @returns DefaultMotionPatch
+ */
 export const rotate = (
 	from: number,
 	to: number | number[]
@@ -22,6 +39,15 @@ export const rotate = (
 	return createPatch({ rotate: from }, { rotate: to });
 };
 
+/**
+ * @description 平移X轴
+ * @param from 初始X轴位置
+ * @param to 目标X轴位置
+ * @example
+ * translateX(0, 12) // { normal: { x: 0 }, animate: { x: 12 } }
+ * translateX(0, [0, 12, 24]) // { normal: { x: 0 }, animate: { x: [0, 12, 24] } }
+ * @returns DefaultMotionPatch
+ */
 export const translateX = (
 	from: number,
 	to: number | number[]
@@ -29,6 +55,15 @@ export const translateX = (
 	return createPatch({ x: from }, { x: to });
 };
 
+/**
+ * @description 平移Y轴
+ * @param from 初始Y轴位置
+ * @param to 目标Y轴位置
+ * @example
+ * translateY(0, 8) // { normal: { y: 0 }, animate: { y: 8 } }
+ * translateY(0, [0, -1, 1, 0]) // { normal: { y: 0 }, animate: { y: [0, -1, 1, 0] } }
+ * @returns DefaultMotionPatch
+ */
 export const translateY = (
 	from: number,
 	to: number | number[]
@@ -36,6 +71,17 @@ export const translateY = (
 	return createPatch({ y: from }, { y: to });
 };
 
+/**
+ * @description 缩放X和Y轴
+ * @param fromX 初始X缩放比例
+ * @param toX 目标X缩放比例
+ * @param fromY 初始Y缩放比例
+ * @param toY 目标Y缩放比例
+ * @example
+ * scaleXY(1, 1.2, 1, 0.8) // { normal: { scaleX: 1, scaleY: 1 }, animate: { scaleX: 1.2, scaleY: 0.8 } }
+ * scaleXY(1, [1, 0.9, 1.1, 1], 1, [1, 1.1, 0.9, 1]) // { normal: { scaleX: 1, scaleY: 1 }, animate: { scaleX: [1, 0.9, 1.1, 1], scaleY: [1, 1.1, 0.9, 1] } }
+ * @returns DefaultMotionPatch
+ */
 export const scaleXY = (
 	fromX: number,
 	toX: number | number[],
--- packages/motion-kit/tests/v1-primitives.test.ts
+++ packages/motion-kit/tests/v1-primitives.test.ts
@@ -1,4 +1,4 @@
-import { fade, rotate, translateX, translateY } from "../src/primitives";
+import { fade, rotate, scaleXY, translateX, translateY } from "../src/primitives";
 import { expect, it, describe } from "vitest";
 
 describe("motion-kit primitives", () => {
@@ -85,6 +85,24 @@ describe("motion-kit primitives", () => {
 			expect(translateY(0, [0, -1, 1, 0])).toEqual({
 				normal: { y: 0 },
 				animate: { y: [0, -1, 1, 0] },
+			});
+		});
+	});
+
+	describe("scaleXY", () => {
+		it("当提供参数时使用参数值作为目标值", () => {
+			expect(scaleXY(1, 1.2, 1, 0.8)).toEqual({
+				normal: { scaleX: 1, scaleY: 1 },
+				animate: { scaleX: 1.2, scaleY: 0.8 },
+			});
+		});
+		it("第二个参数为数组时使用数组中的值作为目标值", () => {
+			expect(scaleXY(1, [1, 0.9, 1.1, 1], 1, [1, 1.1, 0.9, 1])).toEqual({
+				normal: { scaleX: 1, scaleY: 1 },
+				animate: {
+					scaleX: [1, 0.9, 1.1, 1],
+					scaleY: [1, 1.1, 0.9, 1],
+				},
 			});
 		});
 	});
